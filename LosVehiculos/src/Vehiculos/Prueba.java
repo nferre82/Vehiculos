f936ad4b3698aa3cd623912fc49dfdc873622932
Yeray <comodin26@gmail.com>
2019-04-16 10:23:42 +0200

    Cambios realizados en vehiculos

---
+++ LosVehiculos/src/Vehiculos/Prueba.java
@@ -0,0 +1,26 @@
+package Vehiculos;
+
+public class Prueba extends Vehiculo  {
+
+	public Prueba(byte numRuedas) {
+		super(numRuedas);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	private void ver(Vehiculo v) {
+		System.out.println("El vehiculo es de color "+v.getColor());
+		System.out.println("Tiene "+v.getNumRuedas()+" numero de ruedas");
+		System.out.println("Con una cilindrada de"+v.getCilindrada());
+		System.out.println("Y una potencia de"+v.getPotencia());
+		//System.out.println("Tiene "+v.getnumeroPlazas+" numero de plazas");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Motocicleta m1=new Motocicleta("azul");
+		m1.verMoto();
+	}
+
+}
