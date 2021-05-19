a54817c09ac6d94e054e1105cce5f4b8e8410f96
nferre82 <nelsonferre82@gmail.com>
2021-05-19 22:21:16 +0200

    adios ver moto

--- LosVehiculos/src/Vehiculos/Motocicleta.java
+++ LosVehiculos/src/Vehiculos/Motocicleta.java
@@ -40,12 +40,4 @@ public class Motocicleta extends Vehiculo{
 	}
 
 
-	public void verMoto ()
-	{
-		System.out.println("La moto es de color "+getColor());
-		System.out.println("Tiene "+getNumRuedas()+" numero de ruedas");
-		System.out.println("Con una cilindrada de"+getCilindrada());
-		System.out.println("Y una potencia de"+getPotencia());
-		System.out.println("Tiene "+numeroPlazas+" numero de plazas");
-	}
 }
